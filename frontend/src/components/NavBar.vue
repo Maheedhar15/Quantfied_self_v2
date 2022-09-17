cb8a712c3c83ee64e68a868612490a289448c343
Maheedhar15 <maheedharsumson@gmail.com>
2022-09-17 16:20:20 +0530

    Removed unwanted links from nav

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -3,16 +3,7 @@
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item" >
-        <router-link to="/" class="nav-link" tabindex="-1"  >Login</router-link>
-      </li>
-      <li class="nav-item" >
-        <router-link to="/register" class="nav-link" tabindex="-1">Register</router-link>
-      </li>
-      
-    </ul>
+
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item" >
